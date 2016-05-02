9c7a15e7a9f611615eb9211cffd2e6889bc9daaa
tarnas14 <tarnas14@gmail.com>
2016-05-02 16:45:27 +0200

    updating only specific cells based on a subscription is a lot faster

--- grid/src/app.jsx
+++ grid/src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DOM from 'react-dom';
 import io from 'socket.io-client';
+import Rx from 'rxjs/Rx';
 
 import Grid from './grid';
 
@@ -13,23 +14,42 @@ const App = React.createClass({
 
     componentDidMount () {
         const socket = io();
-        socket.on('grid', (gridDefinition) => {
-            console.log('grid update', gridDefinition);
 
-            this.setState(oldState => {
-                const gridToUpdate = oldState.grids.find(grid => grid.name === gridDefinition.name);
-                if (gridToUpdate) {
-                    const gridToUpdateIndex = oldState.grids.indexOf(gridToUpdate);
+        socket.on('newGrid', (gridDefinition) => {
+            console.log('new grid', gridDefinition);
 
-                    return {
-                        grids: [...oldState.grids.slice(0, gridToUpdateIndex), gridDefinition, ...oldState.grids.slice(gridToUpdateIndex + 1)]
-                    };
-                }
+            this.setGrid({
+                ...gridDefinition,
+                gridChangeStream: new Rx.Subject()
+            });
+        });
+
+        socket.on('updateCell', (data) => {
+            const grid = this.state.grids.find(existingGrid => existingGrid.name === data.gridName);
+            if (!grid) {
+                console.error(`tried to update cell on grid ${data.gridName}, which does not exist`);
+
+                return;
+            }
+
+            grid.gridChangeStream.next(data.cell);
+        });
+    },
+
+    setGrid (gridDefinition) {
+        this.setState(oldState => {
+            const gridToUpdate = oldState.grids.find(grid => grid.name === gridDefinition.name);
+            if (gridToUpdate) {
+                const gridToUpdateIndex = oldState.grids.indexOf(gridToUpdate);
 
                 return {
-                    grids: [...oldState.grids, gridDefinition]
+                    grids: [...oldState.grids.slice(0, gridToUpdateIndex), gridDefinition, ...oldState.grids.slice(gridToUpdateIndex + 1)]
                 };
-            });
+            }
+
+            return {
+                grids: [...oldState.grids, gridDefinition]
+            };
         });
     },
 
--- gridder.js
+++ gridder.js
@@ -1,26 +1,53 @@
-'uset strict';
+'use strict';
 const request = require('request');
 
 const port = 3002;
 
-const post = (resource, data) => {
+let lastErrorCode = null;
+
+const post = (resource, data, callback) => {
     request.post({
         url: `http://localhost:${port}/${resource}`,
         json: true,
         body: data
-    }, (error) => {
-        if (error) {
-            console.log(`GRIDDER ERROR: ${JSON.stringify(error)}`);
+    }, (error, response) => {
+        if (!error && response.statusCode === 200) {
+            if (lastErrorCode) {
+                console.log('GRID SERVER back up');
+                lastErrorCode = null;
+            }
+            callback && callback();
+
+            return;
+        }
+
+        if (error && lastErrorCode !== error.code) {
+            lastErrorCode = error.code;
+            console.log(`called GRID SERVER/${resource}`);
+
+            switch (error.code) {
+            case ('ECONNREFUSED'):
+                console.log('GRID SERVER IS NOT READY');
+                break;
+            default:
+                console.log(`GRIDDER ERROR: ${JSON.stringify(error)}`);
+            }
         }
     });
 };
 
 const gridderFactory = (gridName) => {
     return {
-        grid (gridDefinition) {
-            post('grid', {
+        newGrid (gridDefinition, callback) {
+            post('newGrid', {
                 name: gridName,
                 gridDefinition
+            }, callback);
+        },
+        updateCell (cellDefinition) {
+            post('updatecell', {
+                gridName,
+                cell: cellDefinition
             });
         }
     };
