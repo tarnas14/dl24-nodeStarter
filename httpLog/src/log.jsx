c58f6fe0cc0c44c069ce775a29fc7e269e64dd89
tarnas14 <tarnas14@gmail.com>
2016-05-03 14:29:57 +0200

    added type filters to log

--- httpLog/src/log.jsx
+++ httpLog/src/log.jsx
@@ -8,24 +8,27 @@ const Log = React.createClass({
     getInitialState () {
         return {
             selected: null,
-            displayedEntries: this.props.entries.slice(0, 50)
+            displayedEntries: this.props.entries.slice(0, 50),
+            typesToHide: []
         };
     },
 
     componentWillReceiveProps (nextProps) {
-        if (nextProps.entries !== this.state.entries) {
+        if (nextProps.entries !== this.props.entries) {
+            const filteredEntries = nextProps.entries.filter(entry => this.typeIsVisible(entry.type));
+
             if (this.state.selected) {
-                const lastDisplayedIndex = nextProps.entries.indexOf(this.state.displayedEntries[0]);
+                const lastDisplayedIndex = filteredEntries.indexOf(this.state.displayedEntries[0]);
 
                 this.setState({
-                    displayedEntries: nextProps.entries.slice(0, this.state.displayedEntries.length + lastDisplayedIndex)
+                    displayedEntries: filteredEntries.slice(0, this.state.displayedEntries.length + lastDisplayedIndex)
                 });
 
                 return;
             }
 
             this.setState({
-                displayedEntries: nextProps.entries.slice(0, this.state.displayedEntries.length > 50 ? this.state.displayedEntries.length : 50)
+                displayedEntries: filteredEntries.slice(0, this.state.displayedEntries.length > 50 ? this.state.displayedEntries.length : 50)
             });
         }
     },
@@ -67,15 +70,82 @@ const Log = React.createClass({
         });
     },
 
+    getTypes () {
+        const types = this.props.entries.map(entry => entry.type).sort();
+
+        return types.filter((value, index, array) => {
+            return array.indexOf(value) === index;
+        });
+    },
+
+    renderFilters () {
+        return (
+            <div>
+                {
+                    this.getTypes().map(type => (
+                        <div className="checkbox" key={type}>
+                            <label>
+                                <input
+                                    defaultChecked
+                                    onChange={this.setFilter.bind(null, type)}
+                                    type="checkbox"
+                                />
+                                {type}
+                            </label>
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    },
+
+    typeIsVisible (type) {
+        return this.state.typesToHide.indexOf(type) === -1;
+    },
+
+    setFilter (type) {
+        if (this.typeIsVisible(type)) {
+            this.hideType(type);
+
+            return;
+        }
+
+        this.showType(type);
+    },
+
+    hideType (type) {
+        this.setState(oldState => {
+            return {
+                typesToHide: [...oldState.typesToHide, type]
+            };
+        });
+    },
+
+    showType (type) {
+        this.setState(oldState => {
+            const typeIndex = oldState.typesToHide.indexOf(type);
+
+            return {
+                typesToHide: [...oldState.typesToHide.slice(0, typeIndex), ...oldState.typesToHide.slice(typeIndex + 1)]
+            };
+        });
+    },
 
     render () {
         let entriesToDisplay = [...this.state.displayedEntries];
         entriesToDisplay.reverse();
         return (
             <div className="panel-group">
-                {this.state.displayedEntries.length}<br />
-                <button onClick={this.loadPrevious}>previous</button>
-                <button onClick={this.reset}>reset to real time</button>
+                <div className="clearfix">
+                    <div className="pull-left">
+                        {this.state.displayedEntries.length}<br />
+                        <button onClick={this.loadPrevious}>previous</button>
+                        <button onClick={this.reset}>reset to real time</button>
+                    </div>
+                    <div className="pull-right">
+                        {this.renderFilters()}
+                    </div>
+                </div>
                 {entriesToDisplay.map(logEntry => (
                     <div className="panel panel-default">
                         <div className="panel-heading">
